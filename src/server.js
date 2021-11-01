6196111dccd44a7797d3db71fa6fd8f788b32d85
Luis Gabriel <janco7249@gmail.com>
2021-11-01 13:50:47 -0400

    update middleware

--- src/server.js
+++ src/server.js
@@ -1,8 +1,16 @@
 const express = require('express')
+const logger = require('./loggerMiddleware')
+const cors = require('cors')
 const exphbs = require('express-handlebars')
 const path = require('path')
+// const morgan = require('morgan')
+const session = require('express-session')
+const flash = require('connect-flash')
+const passport = require('passport')
+
 // Initializations
 const app = express()
+require('./config/passport')
 
 // Settings
 app.set('port', process.env.PORT || 8000)
@@ -16,14 +24,32 @@ app.engine('.hbs', exphbs({
 app.set('view engine', '.hbs')
 
 // Middlewares
-app.use(express.urlencoded({extended: false})) // convert to json
+app.use(cors())
+app.use(express.json())
+app.use(logger)
+// app.use(express.urlencoded({extended: false})) // convert to json
+// app.use(morgan('dev'))
+app.use(session({
+  secret: 'secret',
+  resave: true,
+  saveUninitialized: true
+}))
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(flash())
 
 // Global Variables
-
+app.use((req, res, next) => {
+  res.locals.error = req.flash('error')
+  res.locals.user = req.user || null
+  next()
+})
 // Routes
 app.use(require('./routes/index.routes'))
+app.use(require('./routes/notes.routes'))
+app.use(require('./routes/users.routes'))
 
 // Static Files
-app.use(express.static(path.join(__dirname, 'public'))) // file public
+// app.use(express.static(path.join(__dirname, 'public'))) // file public
 
 module.exports = app
